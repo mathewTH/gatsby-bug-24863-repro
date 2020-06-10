7105833bd216d4f69aef38f78defc519d1f49c8c
Mathew Tonkin Henwood <7479946+mathewTH@users.noreply.github.com>
2020-06-10 21:07:18 +1200

    Add gatsby config for bug #24863 reproduction

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,27 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
+require("dotenv").config({
+  path: `.env`,
+})
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-source-google-spreadsheet",
+      options: {
+        // https://docs.google.com/spreadsheets/d/<spreadsheetId>/edit#gid=0
+        spreadsheetId: "1_cf5CZKzouzChMQCEEmUvuE_KAn4778Rbzbi9Du8oV0",
+        // The `credentials` are only needed when you need to be authenticated to read the document.
+        // It's an object with the following shape:
+        // {
+        //   client_email: "<your service account email address>",
+        //   private_key: "<the prive key for your service account>"
+        // }
+        //   - https://github.com/googleapis/google-api-nodejs-client#service-to-service-authentication
+        //   - https://developers.google.com/identity/protocols/OAuth2ServiceAccount
+        credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS),
+      },
+    },
+  ],
 }
